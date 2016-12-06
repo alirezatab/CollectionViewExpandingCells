010feced0c5f2f822b217198cd39fc6fcf6d0c51
alirezatab <alirezatabrizi10@gmail.com>
2016-12-05 16:45:10 -0800

    Featuring Featured Cells imlemented by using alpha to darken the standard cells and keeping the featured cell alpha to lowest

--- Ultravisual/InspirationCell.swift
+++ Ultravisual/InspirationCell.swift
@@ -20,5 +20,20 @@ class InspirationCell: UICollectionViewCell {
       }
     }
   }
-  
+    
+    override func apply(_ layoutAttributes: UICollectionViewLayoutAttributes) {
+        super.apply(layoutAttributes)
+        
+        // 1 - These are the two convenience height constants you’ve used previously.
+        let standardHeight = UltravisualLayoutConstants.Cell.standardHeight
+        let featuredHeight = UltravisualLayoutConstants.Cell.featuredHeight
+        
+        // 2 - Calculate the delta of the cell as it’s moving to figure out how much to adjust the alpha in the following step.
+        let delta = 1 - ((featuredHeight - frame.height) / (featuredHeight - standardHeight))
+        
+        // 3 - Based on the range constants, update the cell’s alpha based on the delta value.
+        let minAlpha: CGFloat = 0.3
+        let maxAlpha: CGFloat = 0.75
+        imageCoverView.alpha = maxAlpha - (delta * (maxAlpha - minAlpha))
+    }
 }
